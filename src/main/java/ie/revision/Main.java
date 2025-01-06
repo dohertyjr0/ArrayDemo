02604460478368fad00c5b2aaf5915486a000f2f
doheryjry0 <dohertyjr0@icloud.com>
2025-01-06 11:39:40 +0000

    Added enhanced loop

--- src/main/java/ie/revision/Main.java
+++ src/main/java/ie/revision/Main.java
@@ -1,13 +1,28 @@
 package ie.revision;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         int[] Array = {100, 200, 300, 400, 500, 600, 700, 800, 900, 1000};
 
-        for(int i = 0; i < Array.length; i++) {
-            System.out.println(Array[i]);
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a new value for index 10: ");
+        int newValue = scanner.nextInt();
+
+        try {
+            Array[10] = newValue;
+            System.out.println("Value at index 10: " + Array[10]);
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Error: Index 10 is out of bounds for this array.");
+        }
+
+        public static void printArray(int[] array) {
+            int index = 0;
+            for (int element : array) {
+                System.out.println("Element at index " + index + ": " + element);
+                index++;
+            }
         }
     }
 }

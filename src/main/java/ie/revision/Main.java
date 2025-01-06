2048f505595392b24bec7535b1fb965c252f5c06
doheryjry0 <dohertyjr0@icloud.com>
2025-01-06 11:34:32 +0000

    First commit

---
+++ src/main/java/ie/revision/Main.java
@@ -0,0 +1,13 @@
+package ie.revision;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        int[] Array = {100, 200, 300, 400, 500, 600, 700, 800, 900, 1000};
+
+        for(int i = 0; i < Array.length; i++) {
+            System.out.println(Array[i]);
+        }
+    }
+}
